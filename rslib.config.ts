9936cf1fd62078339288f7c16987f2ae8209be84
liwb <lwbhtml@163.com>
2025-07-17 10:50:12 +0800

    chore: 更新配置文件和依赖版本，调整发布工作流

--- rslib.config.ts
+++ rslib.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@rslib/core';
 export default defineConfig({
   lib: [
     { format: 'esm', syntax: 'es2021', dts: true },
-    { format: 'cjs', syntax: 'es2019' },
+    { format: 'cjs', syntax: 'es2021' },
   ],
 });
